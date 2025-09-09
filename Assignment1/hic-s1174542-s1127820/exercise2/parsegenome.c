4715c5d5ad4a84c0987353b01272b659e020102e
Scholtetje <davidscholten2003@gmail.com>
2025-09-09 14:34:54 +0200

    c bestand klaar

--- Assignment1/hic-s1174542-s1127820/exercise2/parsegenome.c
+++ Assignment1/hic-s1174542-s1127820/exercise2/parsegenome.c
@@ -1,23 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-bool file_correct(filename)
+bool file_correct(char *filename, int counts[])
 {
     FILE* fp = fopen(filename, "r");
-    char ch;
-    bool correct = true;
-    while ((ch = fgetc(fp) != '/n' && ch != EOF))
+    if (!fp)
     {
-        if (ch != 'A' && ch != 'C' && ch != 'G' && ch != 'T')
+        printf("Could not open file %s\n", filename);
+        return false;
+    }
+    char ch;
+    int letter_count = 0;
+    int line_count = 0;
+    bool correct = true;
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        if (letter_count == 100 && ch == '\n')
         {
-            correct = false;
-            return correct;
+            letter_count = 0;
+            line_count++;
+        }
+        else if (letter_count == 100 && ch != '\n')
+        {
+            return false;
+        }
+        else if (ch != 'A' && ch != 'C' && ch != 'G' && ch != 'T')
+        {
+            return false;
+        }
+        else
+        {
+            if (ch == 'A') counts[0]++;
+            if (ch == 'C') counts[1]++;
+            if (ch == 'G') counts[2]++;
+            if (ch == 'T') counts[3]++;
+            letter_count++;
         }
     }
+    fclose(fp);
+    return line_count == 500;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+    int counts[] = {0, 0, 0, 0}; // A, C, G, T
+    bool correct = file_correct(argv[1], counts);
+    printf("File correct: %d\n", correct);
+    if (correct)
+    {
+        printf("A: %d\nC: %d\nG: %d\nT: %d\n", counts[0], counts[1], counts[2], counts[3]);
+        return 0;
+    }
+    return 1;
 }
