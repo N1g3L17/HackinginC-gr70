58c38ec958e1ad1949c760418de6ef67c328c5d7
¨NigelNagel¨ <njwnagel05@gmail.com>
2025-09-30 17:50:38 +0200

    Assignment 4 and working ex1

---
+++ Assignment4/hic-assignment2-s1174542-s1127820/exercise1/buffer.c
@@ -0,0 +1,22 @@
+// vim: shiftwidth=4 tabstop=4 softtabstop=4 noexpandtab
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[1]) {
+	char command[10];
+	char buffer[100];
+	strcpy(command, "/bin/ls");
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s <string>\n", argv[0]);
+		exit(1);
+	}
+
+	strcpy(buffer, argv[1]);
+
+	printf("* [INFO] The value of command is: \"%s\"\n", command);
+
+	system(command);
+
+	return 0;
+}
---
+++ Assignment4/hic-assignment2-s1174542-s1127820/utilities/functions.h
@@ -0,0 +1,9 @@
+// vim: shiftwidth=4 tabstop=4 softtabstop=4 noexpandtab
+#ifndef _HIC_FUNCTIONS_H
+#define _HIC_FUNCTIONS_H
+
+#include <stdint.h>
+
+void print_address_little_endian(uintptr_t address);
+
+#endif
